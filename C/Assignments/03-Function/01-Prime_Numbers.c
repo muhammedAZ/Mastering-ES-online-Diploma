feaad2fdbadfcb4963be3e97a782c7915565c6de
muhammedAZ <muhammedahmed354@gmail.com>
2020-12-13 04:49:13 +0200

    update

---
+++ C/Assignments/03-Function/01-Prime_Numbers.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int check_prime (int num);
+int main(){
+	int num1,num2,i,flag;
+	printf("Enter two numbers(intervals) : ");
+	fflush(stdin);fflush(stdout);
+	scanf("%d%d",&num1,&num2);
+	printf("Prime numbers between 10 and 30 are: ");
+	for(i = num1 + 1 ; i < num2 ; i++)
+	{
+		flag = check_prime(i);
+		if(flag == 0)
+		{
+			printf("%d ",i);
+		}
+	}
+	
+	return 0;
+	
+}
+int check_prime (int num)
+{
+	int j ,flag = 0;
+	 
+	for( j = 2 ;j <= num/2 ;j++)
+	{
+		if(num%j == 0)
+		{
+			flag = 1;
+			break;
+		}
+		
+	}
+	return flag;
+
+}
